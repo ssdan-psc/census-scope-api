44dacd6b4131d5d031e96e9f5da73cc5779e3309
Rachel S Connolly <rshconn@caen-vnc-vm05.engin.umich.edu>
2017-02-17 10:31:25 -0500

    array syntax errors

--- api/build_json.php
+++ api/build_json.php
@@ -3,7 +3,7 @@ function get_cols($topic, $chart, $conn) {
     $query = "SELECT col, label FROM col_map WHERE topic=" . "'" . $topic . "' AND " . $chart . "=1";
     $cols = array();    // cols = [[col0, label0], [col1, label1], ... , [coln, labeln]]
     foreach ($conn->query($query) as $row) {
-    	array_push($cols, ['col' => $row['col'], 'label' => $row['label']]);
+    	array_push($cols, array('col' => $row['col'], 'label' => $row['label']));
     }
 
     return $cols;
